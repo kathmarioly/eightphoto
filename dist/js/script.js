cd8aadc20ba37701bf4efcaf6e5642413d333461
kathmarioly <katerinemarioly@gmail.com>
2017-08-03 21:01:21 -0400

    navbar

--- dist/js/script.js
+++ dist/js/script.js
@@ -12629,3 +12629,33 @@ if (typeof jQuery === 'undefined') {
   })
 
 }(jQuery);
+
+$(document).ready(function () {
+  var trigger = $('.hamburger'),
+  overlay = $('.overlay'),
+  isClosed = false;
+
+  trigger.click(function () {
+    hamburger_cross();      
+  });
+
+  function hamburger_cross() {
+
+    if (isClosed == true) {          
+      overlay.hide();
+      trigger.removeClass('is-open');
+      trigger.addClass('is-closed');
+      isClosed = false;
+    } else {   
+      overlay.show();
+      trigger.removeClass('is-closed');
+      trigger.addClass('is-open');
+      isClosed = true;
+    }
+  }
+  
+  $('[data-toggle="offcanvas"]').click(function () {
+    $('#wrapper').toggleClass('toggled');
+  });  
+});
+
